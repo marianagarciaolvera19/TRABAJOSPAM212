427b11d5f0f46b74a74bc0e18dfd269838a7d355
marianagarciaolvera19 <marianagarciaolvera19@gmail.com>
2025-09-11 16:31:37 -0600

    Mandamos un mensaje al usuario de incorrecto o correcto con if

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -5,5 +5,14 @@ let input = 'dsgsfgsfg'
 
 let result = input == passwordBD;
 
+if (result === true) {
+    console.log('Login correcto');
+
+}
+
+if (result === false) {
+        console.log('Login incorrecto');
+}
+
 console.log(result);
 
