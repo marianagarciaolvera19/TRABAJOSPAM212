47fe7a11bc53f35293f25e425545510de2e61eb7
marianagarciaolvera19 <marianagarciaolvera19@gmail.com>
2025-09-11 10:19:35 -0600

    Aquí nos muestra los diferentes tipos de datos

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -1,2 +1,42 @@
-console.log("Hello World");
-Hello World
+console.log('this is a string');
+
+// Tipos de datos
+"Hello World" //string
+'Hello World' // string
+
+
+100000  // number
+-2.3
+
+// boolean
+true 
+false
+
+//array
+
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+//object
+
+'ryan'
+70.4
+14 
+true 
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+    "friends": [ , , ,]
+}
+
+{
+    "username": 'joe',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": false
+    "friends": [ , , ,]
+}
