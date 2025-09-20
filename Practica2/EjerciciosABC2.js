4b998dad06d465d4dbe3dd770d4877dbc8b9fa31
marianagarciaolvera19 <marianagarciaolvera19@gmail.com>
2025-09-19 22:42:28 -0600

    Ejercicio C: Aquí se usó al .find, .forEach y al .reduce.

--- Practica2/EjerciciosABC2.js
+++ Practica2/EjerciciosABC2.js
@@ -1,20 +1,21 @@
+/*c.
+Ahora con un arreglo de personas, realiza lo siguiente:
+1. Usa .find() para buscar a la persona con nombre "Luis".
+2. Usa .forEach() para imprimir el nombre de cada persona con su edad.
+3. Usa .reduce() para sumar todas las edades y obtener un total.*/
 
-/*b.
-Con el siguiente arreglo de productos, realiza lo siguiente:
-1. Filtra los productos cuyo precio sea mayor a 1000.
-2. Usa .map() para convertir el resultado en un nuevo arreglo con solo los nombres de
-esos productos.*/
-
-const productos = [
-    { nombre: "Laptop", precio: 12000 },
-    { nombre: "Mouse", precio: 250 },
-    { nombre: "Teclado", precio: 750},
-    { nombre: "Monitor", precio: 3000 }
+const personas = [
+    {nombre: "Ana", edad: 22},
+    {nombre: "Luis", edad: 35},
+    {nombre: "María", edad: 28}
 ];
 
-let nombres = productos.filter(producto => producto.precio > 1000);
-console.log("El precio de estos productos es myaor a 1000: ");
-console.log(nombres); 
+let Luis = personas.find(persona => persona.nombre == "Luis");
+console.log(Luis);
 
-let solonombre = nombres.map(filtrado => filtrado.nombre);
-console.log(solonombre);
+personas.forEach (function(persona){
+    console.log(persona.nombre, persona.edad);
+});
+
+let sumar = personas.reduce((Suma, persona) => Suma + persona.edad, 0);
+console.log(sumar);
